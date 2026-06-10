2c447f19b5f38595227608d65afe9234792bfd77
Mohammad Asghari <mohammad.asghari1955@gmail.com>
2026-06-10 17:57:55 +0330

    feat/refactor: add auth router and update module imports

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,13 @@
 import express from "express";
-import { healthRouter } from "./api/health/health.route";
-import { errorMiddleware } from "./middlewares/error-middleware";
+import healthRouter from "./api/health/health.route.js";
+import authRouter from "./api/v1/auth/auth.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 export const app = express();
 
 app.use(express.json());
 
 app.use("/health", healthRouter);
+app.use("/api/v1/auth", authRouter);
 
-
-
-
-
-
-
-
-app.use(errorMiddleware)
+app.use(errorMiddleware);

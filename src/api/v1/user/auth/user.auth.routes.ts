645454e925febfe70992538f8e01004430aded1b
Mohammad Asghari <mohammad.asghari1955@gmail.com>
2026-06-16 00:54:45 +0330

    feat(login): add login route for user and admin

--- src/api/v1/user/auth/user.auth.routes.ts
+++ src/api/v1/user/auth/user.auth.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { registerSchema } from "../../../../shared/validators/auth/register.schema.js";
+import { loginSchema } from "../../../../shared/validators/auth/login.schema.js";
 import asyncHandler from "../../../../shared/utils/async.handler.js";
 import validateBody from "../../../../middlewares/validation.middleware.js";
 import { userAuthController } from "../../../../application/auth/index.js";
@@ -10,6 +11,12 @@ router.post(
   "/register",
   validateBody(registerSchema),
   asyncHandler(userAuthController.register),
+);
+
+router.post(
+  "/login",
+  validateBody(loginSchema),
+  asyncHandler(userAuthController.login),
 );
 
 export default router;

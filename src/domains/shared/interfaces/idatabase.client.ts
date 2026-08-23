bfdc851f91cb177c96c6f7b3d91d1dde177d75d1
Mohammad Asghari <mohammad.asghari1955@gmail.com>
2026-08-23 19:19:49 +0330

    feat: extend database client response with rowCount

--- src/domains/shared/interfaces/idatabase.client.ts
+++ src/domains/shared/interfaces/idatabase.client.ts
@@ -1,5 +1,8 @@
 interface IDatabaseClient {
-  query<T = any>(sql: string, params?: any[]): Promise<{ rows: T[] }>;
+  query<T = any>(
+    sql: string,
+    params?: any[],
+  ): Promise<{ rows: T[]; rowCount: number }>;
 }
 
 export default IDatabaseClient;
--- src/infrastructure/database/pg.database.client.ts
+++ src/infrastructure/database/pg.database.client.ts
@@ -4,9 +4,15 @@ import IDatabaseClient from "../../domains/shared/interfaces/idatabase.client.js
 class PgDatabaseClient implements IDatabaseClient {
   constructor(private client: PoolClient) {}
 
-  async query<T = any>(sql: string, params?: any[]): Promise<{ rows: T[] }> {
+  async query<T = any>(
+    sql: string,
+    params?: any[],
+  ): Promise<{ rows: T[]; rowCount: number }> {
     const result = await this.client.query(sql, params);
-    return result;
+    return {
+      rows: result.rows,
+      rowCount: result.rowCount ?? 0,
+    };
   }
 }
 

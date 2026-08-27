532139b8676a7275336f0b993ae79bad7ec34f75
Mohammad Asghari <mohammad.asghari1955@gmail.com>
2026-08-28 00:00:30 +0330

    perf: remove redundant refresh_token token_hash index

--- src/infrastructure/database/migrations/002.create.refresh.token.table.sql
+++ src/infrastructure/database/migrations/002.create.refresh.token.table.sql
@@ -16,9 +16,6 @@ WHERE revoked_at IS NULL;
 CREATE INDEX idx_refresh_token_user_device_revoked 
 ON refresh_token(user_id, device_id, revoked_at);
 
-CREATE INDEX idx_refresh_token_token_hash 
-ON refresh_token(token_hash);
--- DOWN
 -- DOWN
 DROP INDEX IF EXISTS idx_unique_active_refresh_token_device;
 DROP INDEX IF EXISTS idx_refresh_token_user_device_revoked;

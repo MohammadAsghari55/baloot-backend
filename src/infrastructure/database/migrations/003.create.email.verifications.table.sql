4120a6aaa303e0fc58d87553161c109f68eb056d
Mohammad Asghari <mohammad.asghari1955@gmail.com>
2026-08-27 21:44:18 +0330

    fix: remove updated_at and change code to hashed_code

--- src/infrastructure/database/migrations/003.create.email.verifications.table.sql
+++ src/infrastructure/database/migrations/003.create.email.verifications.table.sql
@@ -2,9 +2,8 @@
 CREATE TABLE IF NOT EXISTS email_verifications (
     id                      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id                 UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    code                    VARCHAR(6) NOT NULL ,
+    hashed_code             TEXT NOT NULL,
     created_at              TIMESTAMPTZ DEFAULT NOW(),
-    updated_at              TIMESTAMPTZ DEFAULT NULL,
     expires_at              TIMESTAMPTZ NOT NULL
 );
 
